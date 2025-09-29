183d33a1e880377cbc034a285f1234f525611d17
Evid3008 <evid3008@gmail.com>
2025-09-29 14:12:13 -0500

    Fixed ImportError: HottyBin -> elvarinBin in stream.py

--- ElvarinXMusic/utils/stream/stream.py
+++ ElvarinXMusic/utils/stream/stream.py
@@ -61,7 +61,7 @@ from ElvarinXMusic.utils.inline import (
     stream_markup2,
     panel_markup_4,
 )
-from ElvarinXMusic.utils.pastebin import HottyBin
+from ElvarinXMusic.utils.pastebin import elvarinBin
 from ElvarinXMusic.utils.stream.queue import put_queue, put_queue_index
 from youtubesearchpython.__future__ import VideosSearch
 
