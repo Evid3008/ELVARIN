3793b658a99fccae04ff5ce6ac07df9cc92ff854
Evid3008 <evid3008@gmail.com>
2025-09-29 09:55:49 -0500

    🔧 Fix: Add SUDO_USERS to config.py for admin filters

--- config.py
+++ config.py
@@ -27,6 +27,9 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "5467532693"))
 
+# Sudo users list
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5467532693").split()))
+
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
