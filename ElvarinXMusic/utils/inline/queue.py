020126c431a640e38f9783f61aa7d8207e4a3a65
Evid3008 <evid3008@gmail.com>
2025-10-17 02:35:03 +0530

    Fix autoplay button overflow - Changed to infinity symbol ∞ for minimal width

--- ElvarinXMusic/utils/inline/queue.py
+++ ElvarinXMusic/utils/inline/queue.py
@@ -73,7 +73,7 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(text="OWNER", url="https://t.me/iq4u8"),
-            InlineKeyboardButton(text="⇄", callback_data="autoplay_mode"),
+            InlineKeyboardButton(text="∞", callback_data="autoplay_mode"),
             InlineKeyboardButton(text="SUPPORT", url="https://t.me/iq4us"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
